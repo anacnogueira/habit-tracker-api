d56a7cab4b069bd0939640c4937c3dd4b2570559
Ana Claudia <anacnogueira@gmail.com>
2026-08-04 15:43:50 -0300

    Formatando codigo com pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
