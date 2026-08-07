5628e52216855659fe861ed62e48b4a18929e77a
Ana Claudia <anacnogueira@gmail.com>
2026-08-07 17:32:02 -0300

    Implementando Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HabitController;
 use App\Http\Controllers\HabitLogController;
 use Illuminate\Support\Facades\Route;
 
 Route::name('api.')->group(function() {
+
+    // Guest
+    Route::middleware('guest')->group(function() {
+        Route::post('/register', [AuthController::class, 'register']);
+    });
+
+    //Authentication
     Route::apiResource('habits', HabitController::class)->scoped(['habit' => 'uuid']);
 
     Route::apiResource('habits.logs', HabitLogController::class)
