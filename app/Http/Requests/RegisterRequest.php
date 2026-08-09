91361a2e9074a60000075344a0871561fb78534f
Ana Claudia <anacnogueira@gmail.com>
2026-08-09 07:44:22 -0300

    Regras para registro de senha

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
 
 class RegisterRequest extends FormRequest
 {
@@ -16,7 +17,11 @@ class RegisterRequest extends FormRequest
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'max:255', 'unique:users,email'],
-            'password' => ['required', 'confirmed'],
+            'password' => [
+                'required',
+                'confirmed',
+                Password::defaults()
+            ],
         ];
     }
 }
